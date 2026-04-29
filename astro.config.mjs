c5a5bc973b4f52e83414b07541c1909952b89b85
danganhuh <mihalevschialexandra@gmail.com>
2026-04-29 17:23:10 +0300

    Remove temporarily the login through the bridge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 
+const base = process.env.BASE_PATH ?? "/";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://danganhuh.github.io",
-  base: "/tum-web-lab2",
+  base,
   output: "static"
 });
