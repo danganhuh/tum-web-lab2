047faacdc643bf944d7e174d08ade7a9bd586c77
Alexandra Mihalevschi <68810700+danganhuh@users.noreply.github.com>
2026-04-29 16:59:54 +0300

    Refactor/astro (#30)
    * Fix astro problem when building
    
    * REfactor the deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://danganhuh.github.io",
+  base: "/tum-web-lab2",
   output: "static"
 });
