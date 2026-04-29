14e32d4b088c2e89c44f073b1d42f1e48babe5e1
Alexandra Mihalevschi <68810700+danganhuh@users.noreply.github.com>
2026-04-29 18:50:29 +0300

    Refactor CMS page addition (#34)
    * Fix astro problem when building
    
    * REfactor the deploy.yml
    
    * Remove temporarily the login through the bridge
    
    * Fix saving to pages
    
    * Change CMS page layout after publishing
    
    * Fix images and video layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 
-const base = process.env.BASE_PATH ?? "/";
+const isProductionBuild = process.env.NODE_ENV === "production";
+const base = process.env.BASE_PATH ?? (isProductionBuild ? "/tum-web-lab2" : "/");
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://danganhuh.github.io",
+  site: "https://danganhuh.github.io/tum-web-lab2",
   base,
   output: "static"
 });
